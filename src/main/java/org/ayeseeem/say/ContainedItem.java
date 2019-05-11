ayeseeem/icm-jtest

Helper to expressively check if item is in a group of items
Adds a helper class to allow more expressive checks for whether an item
is contained in a group of items - collections, arrays, or varargs. For
example,

if (the(month).isOneOf(SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER) {
    // ...
}

instead of if (Arrays.asList(SEPTEMBER, OCTOBER, NOVEMBER,
DECEMBER).contains(month)) {
    // ...
}

src/main/java/org/ayeseeem/jtest/ContainedItem.java
@@ -1,0 +1,78 @@
+package org.ayeseeem.jtest;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * An item that might be contained in some group of elements. Designed to allow
+ * more expressive {@code if} statements - see {@link #the(Object)}.
+ *
+ * @param <T>
+ *            the item type
+ */
+public class ContainedItem<T> {
+    private final T item;
+
+    private ContainedItem(T item) {
+        this.item = item;
+    }
+
+    /**
+     * Creates a contained item. This method provides a way to write more expressive
+     * checks, for example:
+     *
+     * <pre>
+     * if (the(month).isIn(myFavouriteMonths) {
+     *     // ...
+     * }
+     *
+     * if (the(month).isOneOf(SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER) {
+     *     // it's name derives from its Latin ordinal in the Roman calendar
+     * }
+     * </pre>
+     *
+     * @param item
+     *            the item to check
+     * @param <T>
+     *            the item type
+     * @return a new {@code ContainedItem}
+     */
+    public static <T> ContainedItem<T> the(T item) {
+        return new ContainedItem<>(item);
+    }
+
+    /**
+     * Checks whether the item is contained in a Java {@link Collection}.
+     *
+     * @param collection
+     *            the collection to check
+     * @return true if this item is in the collection
+     */
+    public boolean isIn(Collection<T> collection) {
+        return collection.contains(item);
+    }
+
+    /**
+     * Checks whether the item is contained in an array.
+     *
+     * @param array
+     *            an array of Objects, not primitive types
+     * @return true if this item is in the array
+     */
+    public boolean isIn(T[] array) {
+        return isIn(Arrays.asList(array));
+    }
+
+    /**
+     * Checks whether the item is contained in some items.
+     *
+     * @param items
+     *            the items to check
+     * @return true if this item is one of the items
+     */
+    @SafeVarargs
+    public final boolean isOneOf(T... items) {
+        return isIn(Arrays.asList(items));
+    }
+
+}

src/test/java/org/ayeseeem/jtest/ContainedItemTest.java
@@ -1,0 +1,75 @@
+package org.ayeseeem.jtest;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+
+public class ContainedItemTest {
+
+    @Test
+    public void testIsIn_Collection() {
+        Collection<String> collection = Arrays.asList("aaa", "bbb", "ccc");
+
+        assertThat(ContainedItem.the("aaa").isIn(collection), is(true));
+        assertThat(ContainedItem.the("bbb").isIn(collection), is(true));
+        assertThat(ContainedItem.the("ccc").isIn(collection), is(true));
+
+        assertThat(ContainedItem.the("zzz").isIn(collection), is(false));
+    }
+
+    @Test
+    public void testIsIn_Array() {
+        String[] array = new String[] { "aaa", "bbb", "ccc" };
+
+        assertThat(ContainedItem.the("aaa").isIn(array), is(true));
+        assertThat(ContainedItem.the("bbb").isIn(array), is(true));
+        assertThat(ContainedItem.the("ccc").isIn(array), is(true));
+
+        assertThat(ContainedItem.the("zzz").isIn(array), is(false));
+    }
+
+    @Test
+    public void testIsIn_Array_PrimitiveTypesMustBoxed() {
+        Integer[] array = new Integer[] { 111, 222, 333 };
+
+        assertThat(ContainedItem.the(111).isIn(array), is(true));
+        assertThat(ContainedItem.the(222).isIn(array), is(true));
+        assertThat(ContainedItem.the(333).isIn(array), is(true));
+
+        assertThat(ContainedItem.the(888).isIn(array), is(false));
+    }
+
+    @Test
+    public void testIsOneOf() {
+        assertThat(ContainedItem.the("aaa").isOneOf("aaa", "bbb", "ccc"), is(true));
+        assertThat(ContainedItem.the("bbb").isOneOf("aaa", "bbb", "ccc"), is(true));
+        assertThat(ContainedItem.the("ccc").isOneOf("aaa", "bbb", "ccc"), is(true));
+
+        assertThat(ContainedItem.the("zzz").isOneOf("aaa", "bbb", "ccc"), is(false));
+    }
+
+    @Test
+    public void testIsOneOf_HandlesPrimitives() {
+        assertThat(ContainedItem.the(111).isOneOf(111, 222, 333), is(true));
+        assertThat(ContainedItem.the(222).isOneOf(111, 222, 333), is(true));
+        assertThat(ContainedItem.the(333).isOneOf(111, 222, 333), is(true));
+
+        assertThat(ContainedItem.the(888).isOneOf(111, 222, 333), is(false));
+    }
+
+    @Test
+    public void testContainedItem_WrapsTheItem() {
+        Collection<String> collection = Arrays.asList("aaa", "bbb", "ccc");
+
+        String item = "aaa";
+        assertThat(ContainedItem.the(item).isIn(collection), is(true));
+
+        item = "zzz";
+        assertThat(ContainedItem.the(item).isIn(collection), is(false));
+    }
+
+}

src/test/java/org/ayeseeem/jtest/example/ContainedItemExamplesTest.java
@@ -1,0 +1,70 @@
+package org.ayeseeem.jtest.example;
+
+import static java.time.Month.DECEMBER;
+import static java.time.Month.JANUARY;
+import static java.time.Month.NOVEMBER;
+import static java.time.Month.OCTOBER;
+import static java.time.Month.SEPTEMBER;
+import static org.ayeseeem.jtest.ContainedItem.the;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.nio.charset.Charset;
+import java.time.Month;
+import java.util.Collection;
+
+import org.junit.Test;
+
+public class ContainedItemExamplesTest {
+
+    public static class IsInCollectionExample {
+        Collection<String> charsetNames = Charset.availableCharsets().keySet();
+
+        boolean isSupported(String charsetName) {
+            if (the(charsetName).isIn(charsetNames)) {
+                return true;
+            }
+            return false;
+        }
+
+        @Test
+        public void testExampleContainedItem_IsIn_Collection() {
+            assertThat(isSupported("UTF-8"), is(true)); // UTF-8 is always supported
+            assertThat(isSupported("WTF-NoSuchEncoding"), is(false));
+        }
+    }
+
+    public static class IsInArrayExample {
+        Integer[] supportedErrorCodes = new Integer[] { 200, 201, 401, 403, 404 };
+
+        boolean isSupported(int errorCode) {
+            if (the(errorCode).isIn(supportedErrorCodes)) {
+                return true;
+            }
+            return false;
+        }
+
+        @Test
+        public void testExampleContainedItem_IsIn_Array() {
+            assertThat(isSupported(200), is(true));
+            assertThat(isSupported(500), is(false));
+        }
+    }
+
+    public static class IsOneOfExample {
+
+        boolean isLatinNumbered(Month month) {
+            if (the(month).isOneOf(SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER)) {
+                return true;
+            }
+            return false;
+        }
+
+        @Test
+        public void testExampleContainedItem_IsOneOf() {
+            assertThat(isLatinNumbered(SEPTEMBER), is(true));
+            assertThat(isLatinNumbered(JANUARY), is(false));
+        }
+    }
+
+}

ayeseeem/icm-bits-java

src/main/java/org/ayeseeem/say/ContainedItem.java
@@ -1,0 +1,78 @@
+package org.ayeseeem.say;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * An item that might be contained in some group of elements. Designed to allow
+ * more expressive {@code if} statements - see {@link #the(Object)}.
+ *
+ * @param <T>
+ *            the item type
+ */
+public class ContainedItem<T> {
+    private final T item;
+
+    private ContainedItem(T item) {
+        this.item = item;
+    }
+
+    /**
+     * Creates a contained item. This method provides a way to write more expressive
+     * checks, for example:
+     *
+     * <pre>
+     * if (the(month).isIn(myFavouriteMonths) {
+     *     // ...
+     * }
+     *
+     * if (the(month).isOneOf(SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER) {
+     *     // it's name derives from its Latin ordinal in the Roman calendar
+     * }
+     * </pre>
+     *
+     * @param item
+     *            the item to check
+     * @param <T>
+     *            the item type
+     * @return a new {@code ContainedItem}
+     */
+    public static <T> ContainedItem<T> the(T item) {
+        return new ContainedItem<>(item);
+    }
+
+    /**
+     * Checks whether the item is contained in a Java {@link Collection}.
+     *
+     * @param collection
+     *            the collection to check
+     * @return true if this item is in the collection
+     */
+    public boolean isIn(Collection<T> collection) {
+        return collection.contains(item);
+    }
+
+    /**
+     * Checks whether the item is contained in an array.
+     *
+     * @param array
+     *            an array of Objects, not primitive types
+     * @return true if this item is in the array
+     */
+    public boolean isIn(T[] array) {
+        return isIn(Arrays.asList(array));
+    }
+
+    /**
+     * Checks whether the item is contained in some items.
+     *
+     * @param items
+     *            the items to check
+     * @return true if this item is one of the items
+     */
+    @SafeVarargs
+    public final boolean isOneOf(T... items) {
+        return isIn(Arrays.asList(items));
+    }
+
+}

src/test/java/org/ayeseeem/say/ContainedItemTest.java
@@ -1,0 +1,75 @@
+package org.ayeseeem.say;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+
+public class ContainedItemTest {
+
+    @Test
+    public void testIsIn_Collection() {
+        Collection<String> collection = Arrays.asList("aaa", "bbb", "ccc");
+
+        assertThat(ContainedItem.the("aaa").isIn(collection), is(true));
+        assertThat(ContainedItem.the("bbb").isIn(collection), is(true));
+        assertThat(ContainedItem.the("ccc").isIn(collection), is(true));
+
+        assertThat(ContainedItem.the("zzz").isIn(collection), is(false));
+    }
+
+    @Test
+    public void testIsIn_Array() {
+        String[] array = new String[] { "aaa", "bbb", "ccc" };
+
+        assertThat(ContainedItem.the("aaa").isIn(array), is(true));
+        assertThat(ContainedItem.the("bbb").isIn(array), is(true));
+        assertThat(ContainedItem.the("ccc").isIn(array), is(true));
+
+        assertThat(ContainedItem.the("zzz").isIn(array), is(false));
+    }
+
+    @Test
+    public void testIsIn_Array_PrimitiveTypesMustBoxed() {
+        Integer[] array = new Integer[] { 111, 222, 333 };
+
+        assertThat(ContainedItem.the(111).isIn(array), is(true));
+        assertThat(ContainedItem.the(222).isIn(array), is(true));
+        assertThat(ContainedItem.the(333).isIn(array), is(true));
+
+        assertThat(ContainedItem.the(888).isIn(array), is(false));
+    }
+
+    @Test
+    public void testIsOneOf() {
+        assertThat(ContainedItem.the("aaa").isOneOf("aaa", "bbb", "ccc"), is(true));
+        assertThat(ContainedItem.the("bbb").isOneOf("aaa", "bbb", "ccc"), is(true));
+        assertThat(ContainedItem.the("ccc").isOneOf("aaa", "bbb", "ccc"), is(true));
+
+        assertThat(ContainedItem.the("zzz").isOneOf("aaa", "bbb", "ccc"), is(false));
+    }
+
+    @Test
+    public void testIsOneOf_HandlesPrimitives() {
+        assertThat(ContainedItem.the(111).isOneOf(111, 222, 333), is(true));
+        assertThat(ContainedItem.the(222).isOneOf(111, 222, 333), is(true));
+        assertThat(ContainedItem.the(333).isOneOf(111, 222, 333), is(true));
+
+        assertThat(ContainedItem.the(888).isOneOf(111, 222, 333), is(false));
+    }
+
+    @Test
+    public void testContainedItem_WrapsTheItem() {
+        Collection<String> collection = Arrays.asList("aaa", "bbb", "ccc");
+
+        String item = "aaa";
+        assertThat(ContainedItem.the(item).isIn(collection), is(true));
+
+        item = "zzz";
+        assertThat(ContainedItem.the(item).isIn(collection), is(false));
+    }
+
+}

src/test/java/org/ayeseeem/say/example/ContainedItemExamplesTest.java
@@ -1,0 +1,70 @@
+package org.ayeseeem.say.example;
+
+import static java.time.Month.DECEMBER;
+import static java.time.Month.JANUARY;
+import static java.time.Month.NOVEMBER;
+import static java.time.Month.OCTOBER;
+import static java.time.Month.SEPTEMBER;
+import static org.ayeseeem.say.ContainedItem.the;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.nio.charset.Charset;
+import java.time.Month;
+import java.util.Collection;
+
+import org.junit.Test;
+
+public class ContainedItemExamplesTest {
+
+    public static class IsInCollectionExample {
+        Collection<String> charsetNames = Charset.availableCharsets().keySet();
+
+        boolean isSupported(String charsetName) {
+            if (the(charsetName).isIn(charsetNames)) {
+                return true;
+            }
+            return false;
+        }
+
+        @Test
+        public void testExampleContainedItem_IsIn_Collection() {
+            assertThat(isSupported("UTF-8"), is(true)); // UTF-8 is always supported
+            assertThat(isSupported("WTF-NoSuchEncoding"), is(false));
+        }
+    }
+
+    public static class IsInArrayExample {
+        Integer[] supportedErrorCodes = new Integer[] { 200, 201, 401, 403, 404 };
+
+        boolean isSupported(int errorCode) {
+            if (the(errorCode).isIn(supportedErrorCodes)) {
+                return true;
+            }
+            return false;
+        }
+
+        @Test
+        public void testExampleContainedItem_IsIn_Array() {
+            assertThat(isSupported(200), is(true));
+            assertThat(isSupported(500), is(false));
+        }
+    }
+
+    public static class IsOneOfExample {
+
+        boolean isLatinNumbered(Month month) {
+            if (the(month).isOneOf(SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER)) {
+                return true;
+            }
+            return false;
+        }
+
+        @Test
+        public void testExampleContainedItem_IsOneOf() {
+            assertThat(isLatinNumbered(SEPTEMBER), is(true));
+            assertThat(isLatinNumbered(JANUARY), is(false));
+        }
+    }
+
+}